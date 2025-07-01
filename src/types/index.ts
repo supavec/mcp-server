ba87c4c32b6076c13e18347b1662feb5369476b0
taishikato <taishi.k0903@gmail.com>
2025-07-01 15:42:04 -0400

    Add UserFile types and request/response structures to index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,29 @@ export type EMBEDDINGS = {
     content: string;
   }[];
 };
+
+export type UserFile = {
+  type: string;
+  file_id: string;
+  created_at: string;
+  file_name: string;
+  team_id: string;
+};
+
+export type UserFilesResponse = {
+  success: boolean;
+  results: UserFile[];
+  pagination: {
+    offset: number;
+    limit: number;
+  };
+  count: number;
+};
+
+export type UserFilesRequest = {
+  pagination?: {
+    limit?: number;
+    offset?: number;
+  };
+  order_dir?: "desc" | "asc";
+};
